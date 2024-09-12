ba85720bcda20d44b2c469bf11014369726d7736
Andre <andrehsandim@gmail.com>
2024-09-12 11:43:34 -0300

    commit jornada de dados

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 42
+nome = "André"
+
+# Prints
+print (idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista 
+lista = [1, 2, 3, 4, 5]
+
+# Métodos
+lista_invertidade = lista.reverse()
+print(lista_invertidade)
